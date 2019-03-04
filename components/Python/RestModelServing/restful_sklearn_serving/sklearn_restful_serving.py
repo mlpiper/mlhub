27ea73217ff031ba5e50404db108fc7c5f626432
Zohar Mizrahi <zohar.mizrahi@parallelmachines.com>
2019-03-04 17:19:32 +0200

    [REF-5699] Add some aggr ML metrics to sklearn rest model serving component

--- components/Python/RestModelServing/restful_sklearn_serving/sklearn_restful_serving.py
+++ components/Python/RestModelServing/restful_sklearn_serving/sklearn_restful_serving.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import logging
 import numpy as np
 import os
@@ -9,6 +10,14 @@ import warnings
 
 from parallelm.components.restful.flask_route import FlaskRoute
 from parallelm.components.restful_component import RESTfulComponent
+from parallelm.components.restful.metric import Metric, MetricType, MetricRelation
+
+
+class ModelType(Enum):
+    other = 1
+    classifier = 2
+    regressor = 3
+    clusteror = 4
 
 
 class SklearnRESTfulServing(RESTfulComponent):
@@ -17,9 +26,21 @@ class SklearnRESTfulServing(RESTfulComponent):
     def __init__(self, engine):
         super(SklearnRESTfulServing, self).__init__(engine)
         self._model = None
+        self._model_type = ModelType.other
         self._model_loading_error = None
         self._params = {}
         self._verbose = self._logger.isEnabledFor(logging.DEBUG)
+        self._num_predictable_classes = 0
+        self._confidence_metric_per_class = []
+        self._num_predictions_metric_per_class = []
+
+        self._low_confidence_threshold_percent = None
+
+        # Metrics
+        self._num_confidences_below_threshold = None
+        self._total_confidence_metric = None
+        self._confidence_bar_graph_metric = None
+        self._prediction_distribution_bar_graph_metric = None
 
         self.info_json = {
             "sample_keyword": SklearnRESTfulServing.JSON_KEY_NAME,
@@ -27,6 +48,60 @@ class SklearnRESTfulServing(RESTfulComponent):
             "numpy": np.version.version,
             "sklearn": sklearn.__version__,
         }
+
+    def configure(self, params):
+        """
+        @brief      It is called in within the 'deputy' context
+        """
+        self._logger.info("Configure component with input params, name: {}, params: {}"
+                          .format(self.name(), params))
+        self._params = params
+
+        self._total_confidence_metric = Metric("total.confidence",
+                                               title="Average Confidence",
+                                               metric_type=MetricType.COUNTER_PER_TIME_WINDOW,
+                                               value_type=float,
+                                               metric_relation=MetricRelation.AVG_PER_REQUEST)
+
+        self._num_predictable_classes = self._params.get("num_predictable_classes", 0)
+        if self._num_predictable_classes > 0:
+
+            # Prediction distribution bar graph
+            self._prediction_distribution_bar_graph_metric = Metric("prediction_distribution",
+                                                                    title="Prediction Distribution",
+                                                                    metric_type=MetricType.COUNTER_PER_TIME_WINDOW,
+                                                                    metric_relation=MetricRelation.BAR_GRAPH,
+                                                                    related_metric=[])
+            for iii in range(self._num_predictable_classes):
+                metric = Metric("num.prediction.per.class.{}".format(iii),
+                                hidden=True,
+                                metric_type=MetricType.COUNTER_PER_TIME_WINDOW)
+                self._num_predictions_metric_per_class.append(metric)
+                self._prediction_distribution_bar_graph_metric.add_related_metric((metric, "{}".format(iii)))
+
+            # Confidence bar graph
+            self._confidence_bar_graph_metric = Metric("confidence_bar_graph",
+                                                       title="Average Confidence per class",
+                                                       metric_type=MetricType.COUNTER_PER_TIME_WINDOW,
+                                                       metric_relation=MetricRelation.BAR_GRAPH,
+                                                       related_metric=[])
+
+            for iii in range(self._num_predictable_classes):
+                metric = Metric("total.confidence.per.class.{}".format(iii),
+                                hidden=True,
+                                metric_type=MetricType.COUNTER_PER_TIME_WINDOW,
+                                value_type=float,
+                                metric_relation=MetricRelation.DIVIDE_BY,
+                                related_metric=self._num_predictions_metric_per_class[iii])
+                self._confidence_metric_per_class.append(metric)
+                self._confidence_bar_graph_metric.add_related_metric((metric, "{}".format(iii)))
+
+        self._low_confidence_threshold_percent = self._params.get("low_confidence_threshold_percent", 0)
+        if self._low_confidence_threshold_percent > 0:
+            self._num_confidences_below_threshold = Metric("num.confidence.below.thrsh",
+                                                           title="Number of predictions with confidence below {}% threshold"
+                                                           .format(self._low_confidence_threshold_percent),
+                                                           metric_type=MetricType.COUNTER_PER_TIME_WINDOW)
 
     def load_model_callback(self, model_path, stream, version):
         self._logger.info(sys.version_info)
@@ -65,6 +140,16 @@ class SklearnRESTfulServing(RESTfulComponent):
         #  b) model loading failed but it can be ignored
         if model is not None:
             self._model = model
+            self._update_model_type()
+
+    def _update_model_type(self):
+        if self._model:
+            if sklearn.base.is_classifier(self._model) or getattr(self._model, "_estimator_type", None) == "clusterer":
+                self._model_type = ModelType.classifier
+            elif sklearn.base.is_regressor(self._model):
+                self._model_type = ModelType.regressor
+            else:
+                self._model_type = ModelType.other
 
     def _empty_predict(self):
         model_loaded = True if self._model else False
@@ -112,13 +197,45 @@ class SklearnRESTfulServing(RESTfulComponent):
         else:
             try:
                 two_dim_array = np.array([form_params[SklearnRESTfulServing.JSON_KEY_NAME]])
-                prediction = self._model.predict(two_dim_array)
+                pred_probs = None
+                try:
+                    pred_probs = self._model.predict_proba(two_dim_array)[0]
+                except:
+                    prediction = self._model.predict(two_dim_array)[0]
+
+                if pred_probs is not None:
+                    pred_index = np.argmax(pred_probs)
+                    prediction = self._model.classes_[pred_index]
+                    prediction_confidence = pred_probs[pred_index]
+
+                    self._logger.debug("pred_probs: {}, pred_index: {}, prediction: {}, confidence: {}"
+                                       .format(pred_probs,  pred_index, prediction, prediction_confidence))
+
+                    # Total confidence
+                    self._total_confidence_metric.increase(prediction_confidence)
+
+                    if self._num_predictable_classes:
+                        # Prediction confidence per class
+                        # index = int(prediction * Metric.FLOAT_PRECISION) % self._num_predictable_classes
+                        index = int(prediction)
+                        self._confidence_metric_per_class[index].increase(prediction_confidence)
+
+                    # Lower probability threshold
+                    if self._low_confidence_threshold_percent and \
+                            prediction_confidence * 100 < self._low_confidence_threshold_percent:
+                        self._num_confidences_below_threshold.increase(1)
+
+                if self._model_type == ModelType.classifier and self._num_predictable_classes:
+                    # index = int(prediction * Metric.FLOAT_PRECISION) % self._num_predictable_classes
+                    index = int(prediction)
+                    self._num_predictions_metric_per_class[index].increase(1)
+
                 if self._verbose:
                     self._logger.debug("predict, url_params: {}, form_params: {}".format(url_params, form_params))
                     self._logger.debug("type<form_params>: {}\n{}".format(type(form_params), form_params))
                     self._logger.debug("type(two_dim_array): {}\n{}".format(type(two_dim_array), two_dim_array))
-                    self._logger.debug("prediction: {}, type: {}".format(prediction[0], type(prediction[0])))
-                return 200, {"prediction": prediction[0]}
+                    self._logger.debug("prediction: {}, type: {}".format(prediction, type(prediction)))
+                return 200, {"prediction": prediction}
             except Exception as e:
                 error_json = {"error": "Error performing prediction: {}".format(e)}
                 error_json.update(self.info_json)
