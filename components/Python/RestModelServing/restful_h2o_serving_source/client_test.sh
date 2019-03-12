8744794d17843d3be14905c61bd0cb84c7fd6adb
Yakov Goldberg <yakov.goldberg@parallelmachines.com>
2019-03-12 14:55:41 -0700

    [REF-5833] move mlops/mlcomp under mlpiper

--- components/Python/RestModelServing/restful_h2o_serving_source/client_test.sh
+++ components/Python/RestModelServing/restful_h2o_serving_source/client_test.sh
@@ -3,6 +3,6 @@
 script_name=$(basename ${BASH_SOURCE[0]})
 script_dir=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
-PYTHONPATH="../:$HOME/dev/reflex/sub/reflex-algos/target/mlcomp-py2.egg:$HOME/dev/reflex/sub/reflex-algos/target/mlops-py2.egg"
+PYTHONPATH="../:$HOME/dev/reflex/sub/mlpiper/mlcomp/dist/mlcomp-py2.egg:$HOME/dev/reflex/sub/mlpiper/mlops/dist/mlops-py2.egg"
 
 PYTHONPATH=$PYTHONPATH python h2o_restful_serving.py 8888 $script_dir/model/GBM_model_python_1542317998658_5.zip
