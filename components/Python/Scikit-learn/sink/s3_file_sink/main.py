01df053ff54e2038c4ce27408d0f9ff9cb2f219b
Dhananjoy Das <joy.das@parallelm.com>
2019-03-12 18:02:41 -0700

    [REF-5826] Update S3 Source/Sink components to use EE

--- components/Python/Scikit-learn/sink/s3_file_sink/main.py
+++ components/Python/Scikit-learn/sink/s3_file_sink/main.py
@@ -21,30 +21,39 @@ class S3FileSink(ConnectableComponent):
         self._save_file(file_path)
         return []
 
+    def _merge_params(self):
+        # User specified params override EE params, so any missing user params need to be fetched from EE.
+        # if you choose to change the policy using EE to override, simply change the order of the following two lines
+        self.ee_params = os.environ.copy()
+        self.ee_params.update(self._params)
+        self._params = self.ee_params
+        return
+
     def _save_file(self, file_path):
+        self._merge_params()
 
         # Initialize mlops
         mlops.init()
 
-        if self._params["get_save_file_size"]:
+        if self._params["sink_get_save_file_size"]:
             file_size = os.stat(file_path).st_size / (1024 * 1024)
             mlops.set_stat("s3.outputFileSizeMB", file_size)
 
-        if self._params["get_save_line_count"]:
+        if self._params["sink_get_save_line_count"]:
             line_count = len(open(file_path).readlines())
             mlops.set_stat("s3.outputFileLineCount", line_count)
 
         client = boto3.client(
             's3',
-            aws_access_key_id=self._params["aws_access_key_id"],
-            aws_secret_access_key=self._params["aws_secret_access_key"],
+            aws_access_key_id=self._params["sink_aws_access_key_id"],
+            aws_secret_access_key=self._params["sink_aws_secret_access_key"],
         )
 
         data = open(file_path, 'rb')
         save_start_time = time.time()
-        client.put_object(Bucket=self._params["bucket"], Key=self._params["key"], Body=data)
+        client.put_object(Bucket=self._params["sink_bucket"], Key=self._params["sink_key"], Body=data)
         save_elapsed_time = time.time() - save_start_time
-        if self._params["get_save_time"]:
+        if self._params["sink_get_save_time"]:
             mlops.set_stat("s3.outputSaveTimemsec", save_elapsed_time)
 
         return
