3be6e8bdce7084ed55d8c9fb19f1ce92d9b39dd0
Dhananjoy Das <joy.das@parallelm.com>
2019-03-12 18:02:41 -0700

    [REF-5826] Update S3 Source/Sink components to use EE

--- components/Python/Scikit-learn/source/s3_file_source/main.py
+++ components/Python/Scikit-learn/source/s3_file_source/main.py
@@ -21,34 +21,43 @@ class S3FileSource(ConnectableComponent):
         file_path = self._fetch_file()
         return [file_path]
 
+    def _merge_params(self):
+        # User specified params override EE params, so any missing user params need to be fetched from EE.
+        # if you choose to change the policy using EE to override, simply change the order of the following two lines
+        self.ee_params = os.environ.copy()
+        self.ee_params.update(self._params)
+        self._params = self.ee_params
+        return
+
     def _fetch_file(self):
         file_size = line_count = 0
+        self._merge_params()
 
         # Initialize mlops
         mlops.init()
 
         client = boto3.client(
             's3',
-            aws_access_key_id=self._params["aws_access_key_id"],
-            aws_secret_access_key=self._params["aws_secret_access_key"],
+            aws_access_key_id=self._params["src_aws_access_key_id"],
+            aws_secret_access_key=self._params["src_aws_secret_access_key"],
         )
 
-        if self._params["get_file_size"]:
-            resp_obj = client.head_object(Bucket=self._params["bucket"], Key=self._params["key"])
+        if self._params["src_get_file_size"]:
+            resp_obj = client.head_object(Bucket=self._params["src_bucket"], Key=self._params["src_key"])
             file_size = resp_obj['ContentLength'] / (1024 * 1024)
             mlops.set_stat("s3.inputFileSizeMB", file_size)
 
         file_path = os.path.join(self._params["parent_directory"], "s3_file_" + str(uuid.uuid4()))
 
         fetch_start_time = time.time()
-        client.download_file(self._params["bucket"], self._params["key"], file_path)
+        client.download_file(self._params["src_bucket"], self._params["src_key"], file_path)
         fetch_elapsed_time = time.time() - fetch_start_time
-        if self._params["get_fetch_time"]:
+        if self._params["src_get_fetch_time"]:
             mlops.set_stat("s3.inputFetchTimemsec", fetch_elapsed_time)
 
         # get line-count for the file (loads file in memory)
         # should help keep prediction latency NOT be IO-bound
-        if self._params["get_line_count"]:
+        if self._params["src_get_line_count"]:
             line_count = len(open(file_path).readlines())
             mlops.set_stat("s3.inputFileLineCount", line_count)
 
