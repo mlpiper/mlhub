798809c0b7f4cb82f07e2edcb12c43217bcfa56b
harshil <harshil2493@gmail.com>
2019-04-15 18:37:27 -0700

    [REF-6034] MultiClass SVC: Adding MultiClass SVC Algo
    - Outputting Stat Using,
    
    mlops.set_stat(ClassificationMetrics.CONFUSION_MATRIX, cm, labels=labels_ordered)
    
    OR
    
    mlops.metrics.confusion_matrix(y_true=labels_actual, y_pred=labels_pred, labels=labels_ordered)

---
+++ components/Python/Scikit-learn/multi-class-svc-train/svc_train.py
@@ -0,0 +1,141 @@
+import argparse
+
+import numpy as np
+from parallelm.mlops import StatCategory as st
+from parallelm.mlops import mlops as mlops
+from parallelm.mlops.stats.bar_graph import BarGraph
+from parallelm.mlops.stats.table import Table
+from sklearn.datasets import make_classification
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+
+
+def parse_args():
+    """
+    Parse Arguments from component
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_samples", help="# samples")
+    parser.add_argument("--num_features", help="# features")
+    parser.add_argument("--C", help="C Parameter")
+    parser.add_argument("--kernel", help="Kernel")
+    parser.add_argument("--degree", help="Degree")
+    parser.add_argument("--gamma", help="Gamma")
+    parser.add_argument("--tol", help="Tol")
+    parser.add_argument("--max_iter", dest="max_iter", type=int, required=False, default=100,
+                        help='Maximum number of iterations')
+    parser.add_argument("--output-model", help="Data file to save model")
+    options = parser.parse_args()
+    return options
+
+
+def main():
+    pm_options = parse_args()
+
+    print("PM: Configuration:")
+    print("PM: # Sample:                    [{}]".format(pm_options.num_samples))
+    print("PM: # Features:                  [{}]".format(pm_options.num_features))
+
+    print("PM: C:                           [{}]".format(pm_options.C))
+    print("PM: Kernel:                      [{}]".format(pm_options.kernel))
+    print("PM: Degree:                      [{}]".format(pm_options.degree))
+    print("PM: Gamma:                       [{}]".format(pm_options.gamma))
+    print("PM: Tolerance:                   [{}]".format(pm_options.tol))
+    print("PM: Maximum iterations:          [{}]".format(pm_options.max_iter))
+
+    print("PM: Output model:                [{}]".format(pm_options.output_model))
+
+    # Initialize MLOps Library
+    mlops.init()
+
+    num_samples = int(pm_options.num_samples)
+    num_features = int(pm_options.num_features)
+    # Create synthetic data using scikit learn
+    X, y = make_classification(n_samples=num_samples,
+                               n_features=num_features,
+                               n_informative=2,
+                               n_redundant=1,
+                               n_classes=3,
+                               n_clusters_per_class=1,
+                               random_state=42)
+
+    # Separate into features and labels
+    features = X
+    labels = y
+
+    # Add noise to the data
+    noisy_features = np.random.uniform(0, 10) * \
+                     np.random.normal(0, 1,
+                                      (num_samples, num_features))
+    features = features + noisy_features
+
+    # Create a model that should be deployed into production
+    final_model = SVC(C=float(pm_options.C),
+                      kernel=pm_options.kernel,
+                      degree=int(pm_options.degree),
+                      gamma=str(pm_options.gamma),
+                      tol=float(pm_options.tol),
+                      max_iter=int(pm_options.max_iter))
+
+    final_model.fit(features, labels)
+
+    # Accuracy for the chosen model
+    accuracy = final_model.score(features, labels)
+    print("Accuracy values: \n {0}".format(accuracy))
+
+    # Label distribution in training
+    value, counts = np.unique(labels, return_counts=True)
+    label_distribution = np.asarray((value, counts)).T
+    column_names = value.astype(str).tolist()
+    print("Label distributions: \n {0}".format(label_distribution))
+
+    # Output label distribution as a BarGraph using MCenter
+    bar = BarGraph().name("Label Distribution").cols((label_distribution[:, 0]).astype(str).tolist()).data(
+        (label_distribution[:, 1]).tolist())
+    mlops.set_stat(bar)
+
+    # Output accuracy of the chosen model using MCenter
+    mlops.set_stat("Accuracy", accuracy, st.TIME_SERIES)
+
+    # Output Health Statistics to MCenter
+    # MLOps API to report the distribution statistics of each feature in the data
+    mlops.set_data_distribution_stat(features)
+
+    labels_pred = final_model.predict(features)
+
+    labels_ordered = sorted(set(labels))
+    cm = confusion_matrix(labels, labels_pred, labels=labels_ordered)
+
+    #################### FIRST WAY ####################
+    # Output Confusion Matrix
+    labels_string = [str(i) for i in labels_ordered]
+    cm_matrix = Table().name("User Given Confusion Matrix").cols(labels_string)
+
+    for index in range(len(cm)):
+        cm_matrix.add_row(labels_string[index], list(cm[index]))
+
+    mlops.set_stat(cm_matrix)
+    #################### DONE FIRST WAY ####################
+
+    #################### NEW WAY ####################
+    # Second Way
+    # mlops.set_stat(ClassificationMetrics.CONFUSION_MATRIX, cm, labels=labels_ordered)
+
+    # OR
+
+    # Third Way
+    mlops.metrics.confusion_matrix(y_true=labels, y_pred=labels_pred, labels=labels_ordered)
+    #################### DONE NEW WAY ####################
+
+    # Save the model
+    import pickle
+    model_file = open(pm_options.output_model, 'wb')
+    pickle.dump(final_model, model_file)
+    model_file.close()
+    # Terminate MLOPs
+    mlops.done()
+
+
+if __name__ == "__main__":
+    main()
